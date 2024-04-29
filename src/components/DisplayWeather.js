d36e3211463c45c718d8f11d77bd83a43bb47e2c
Ganesh Murthy <orangewhitegreen@hotmail.com>
2024-04-29 13:48:33 -0400

    new properties added by krishna

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,18 +1,30 @@
 import './DisplayWeather.css';
-
+var measurement = "";
 function DisplayWeather({weatherResult, cbRemoveWeather}) 
 {  
-   
+  if(weatherResult.sys.country == 'US')
+  {
+    measurement = "F"
+  }
+  else if(weatherResult.sys.country !== 'US') { 
+    measurement = "C";
+  }
     return (
       <div>        
         <div className="card" onClick={() => { cbRemoveWeather(weatherResult.UUID)}} >        
           <div className="title">{weatherResult.name} <a href="#" className="close-thik"></a></div>
           <div className="features">
             <ul>
-              <li>{weatherResult.main.temp}</li>
+              <li>Country: {weatherResult.sys.country} <img src={`https://flagsapi.com/${weatherResult.sys.country}/flat/16.png`}></img></li>
+              <li>{weatherResult.main.temp} °{measurement}</li>
+              <li>Feels like: {weatherResult.main.feels_like}</li>
+              <br />
+              <li>High: {weatherResult.main.temp_max}</li>
+              <li>Low: {weatherResult.main.temp_min}</li>
+              <br />
               <li>{weatherResult.weather[0].main}</li>
               <li><img src={ `http://openweathermap.org/img/w/${weatherResult.weather[0].icon}.png`}></img></li>
-              <li>{weatherResult.weather[0].description}</li>              
+              <li>{weatherResult.weather[0].description}</li>      
             </ul>
           </div>          
         </div>
