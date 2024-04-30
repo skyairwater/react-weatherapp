eb57027f57daa5b65d55d03d9e72edebf8969236
Ganesh Murthy <orangewhitegreen@hotmail.com>
2024-04-30 16:22:55 -0400

    changed the background to gradient

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -2,16 +2,26 @@ import logo from '../logo.svg';
 import Alert from 'react-bootstrap/Alert';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import '../components/SiteHeader.css';
 
 function SiteHeader()
 {
     return(
         <>
-            <Alert variant='danger'>                
+            <Alert variant='warning'>                
                 <Row>
                     <Col md="auto" className='text-center'><h4>Weather App by Krishna Murthy - React Functional Components</h4></Col>
-                    <Col><img src={logo} className="App-logo" alt="logo" /></Col>        
+                    <Col><img src={logo} className="App-logo" alt="logo" /></Col>    
+                    <Col>
+                    <Form.Label htmlFor="exampleColorInput">Site Background</Form.Label>
+                        <Form.Control
+                            type="color"
+                            id="exampleColorInput"
+                            defaultValue="#563d7c"
+                            title="Choose your color"
+                        />
+                    </Col>    
                 </Row>
             </Alert>
         </>
